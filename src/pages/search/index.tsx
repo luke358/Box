5d16412006ecf623bb5b90dfc1b9fa51842a00e2
luke358 <1494135711@qq.com>
2023-06-20 15:12:06 +0800

    feat: search

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,44 +1,33 @@
-import StatusBar from '@/components/base/statusBar';
 import {useNavigate} from '@/entry/router';
 import rpx from '@/utils/rpx';
 import React, {useContext, useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Keyboard, StyleSheet, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 
 import {
+  Appbar,
   Avatar,
   Card,
+  Searchbar,
   Text,
-  TextInput,
   TouchableRipple,
 } from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {WebViewMessageEvent} from 'react-native-webview';
 import {WebviewContext} from '@/entry';
 import Loading from '@/components/base/loading';
-import {useFocusEffect} from '@react-navigation/native';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import PluginManager from '@/core/plugins';
-
-const styles = StyleSheet.create({
-  appWrapper: {
-    flexDirection: 'column',
-    height: '100%',
-    backgroundColor: '#f6f6f6',
-  },
-  view: {
-    paddingHorizontal: rpx(30),
-    paddingTop: rpx(20),
-    // backgroundColor: 'white',
-  },
-  drawerStyle: {
-    backgroundColor: '#f6f6f6',
-    elevation: 15,
-  },
-});
+import useColors from '@/hooks/useColors';
+import useTextColor from '@/hooks/useTextColor';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   const webviewContext = useContext(WebviewContext);
 
+  const colors = useColors();
+  const textColor = useTextColor();
+  const navigation = useNavigation();
   const navigate = useNavigate();
   const [html, setHtml] = useState<any>();
   const [kw, setKw] = useState('');
@@ -90,15 +79,36 @@ export default function App() {
   });
 
   return (
-    <SafeAreaView style={styles.appWrapper}>
-      <StatusBar />
-      <View>
-        <TextInput
-          value={text}
-          onChangeText={_text => setText(_text)}
-          onSubmitEditing={onSubmitEditing}
+    <SafeAreaView style={styles.appWrapper} edges={['bottom', 'top']}>
+      <Appbar style={[styles.appbar, {backgroundColor: colors.primary}]}>
+        <Appbar.BackAction
+          color={textColor}
+          onPress={() => {
+            // !!这个组件有bug，输入法拉起的时候返回会默认打开panel
+            Keyboard.dismiss();
+            navigation.goBack();
+          }}
         />
-      </View>
+        <Searchbar
+          autoFocus
+          inputStyle={styles.input}
+          style={styles.searchBar}
+          onFocus={() => {
+            // setPageStatus(PageStatus.EDITING);
+          }}
+          placeholder="输入要搜索的视频"
+          onSubmitEditing={onSubmitEditing}
+          onChangeText={_text => setText(_text)}
+          value={text}
+        />
+        <Icon
+          style={{marginHorizontal: rpx(20)}}
+          size={30}
+          color={textColor}
+          name="clipboard-text-clock-outline"
+        />
+      </Appbar>
+
       <ScrollView>
         {kw && <Text>当前搜索: {kw}</Text>}
         <View>
@@ -129,3 +139,30 @@ export default function App() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  appWrapper: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: '#f6f6f6',
+  },
+  appbar: {
+    shadowColor: 'transparent',
+  },
+  searchBar: {
+    minWidth: rpx(375),
+    flex: 1,
+    borderRadius: rpx(64),
+    height: '68%',
+    overflow: 'hidden',
+    color: '#666666',
+  },
+  input: {
+    padding: 0,
+    paddingBottom: rpx(22),
+    color: '#666666',
+    fontSize: rpx(26),
+    textAlignVertical: 'center',
+    includeFontPadding: false,
+  },
+});
---
+++ src/pages/search/store/atoms.ts
@@ -0,0 +1,54 @@
+// import {RequestStateCode} from '@/constants/commonConst';
+import {atom} from 'jotai';
+
+/** 搜索状态 */
+
+// export interface ISearchResult<T extends ICommon.SupportMediaType> {
+//     /** 当前页码 */
+//     page?: number;
+//     /** 搜索词 */
+//     query?: string;
+//     /** 搜索状态 */
+//     state: RequestStateCode;
+//     /** 数据 */
+//     data: ICommon.SupportMediaItemBase[T][];
+// }
+
+// type ISearchResults<
+//     T extends keyof ICommon.SupportMediaItemBase = ICommon.SupportMediaType,
+// > = {
+//     [K in T]: Record<string, ISearchResult<K>>;
+// };
+
+/** 初始值 */
+// export const initSearchResults: ISearchResults = {
+//     music: {},
+//     album: {},
+//     artist: {},
+//     // lyric: {}
+// };
+
+/** key: pluginhash value: searchResult */
+// const searchResultsAtom = atom(initSearchResults);
+
+export enum PageStatus {
+  /** 编辑中 */
+  EDITING = 'EDITING',
+  /** 搜索中 */
+  SEARCHING = 'SEARCHING',
+  /** 有结果 */
+  RESULT = 'RESULT',
+  /** 没有安装插件 */
+  NO_PLUGIN = 'NO_PLUGIN',
+}
+
+/** 当前正在搜索的 */
+const pageStatusAtom = atom<PageStatus>(PageStatus.EDITING);
+
+const queryAtom = atom<string>('');
+
+export {
+  pageStatusAtom,
+  //  searchResultsAtom,
+  queryAtom,
+};
