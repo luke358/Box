f7c76bd855ab081bea3b87c545f308473457d168
luke358 <1494135711@qq.com>
2023-06-20 14:17:01 +0800

    chore: code

--- src/core/plugins.ts
+++ src/core/plugins.ts
@@ -153,55 +153,6 @@ class PluginMethods implements IPlugin.IPluginInstanceMethods {
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-let funcCode = `
-module.exports = {
-  platform: '樱花动漫',
-  searchInjectCode: \`
-        const lis = document.querySelectorAll('li.item')
-        const data = Array.from(lis).map(li => {
-          const href = li.querySelector('a').href;
-          const imgblock = li.querySelector('.imgblock');
-          const computedStyle = getComputedStyle(imgblock);
-          const backgroundImage = computedStyle.getPropertyValue('background-image');
-          
-          const urlStartIndex = backgroundImage.indexOf('url("') + 5;
-          const urlEndIndex = backgroundImage.lastIndexOf('")');
-          const pic = backgroundImage.slice(urlStartIndex, urlEndIndex);
-      
-          const title = li.querySelector('.itemtext').textContent
-          return {
-            href,pic,title
-          }
-        });
-        // alert(document.documentElement.innerHTML);
-        window.ReactNativeWebView.postMessage(JSON.stringify({data, isEnd: true}));
-  \`,
-    searchUrl: 'https://www.yhdmz.org/s_all?ex=1&kw={{kw}}', 
-    searchComplete: ({result}) => JSON.parse(result),
-    detailInjectCode: \`
-      const uls = document.querySelectorAll('#play_tabs .main0 .movurl ul')
-    
-      const data = Array.from(uls).map(ul => {
-        const als = ul.querySelectorAll('li a')
-      return {
-        title: 'xxx',
-        data: Array.from(als).map(a => ({href: a.href, title: a.textContent}))
-      }
-      })
-    
-      window.ReactNativeWebView.postMessage(JSON.stringify(data));
-      \`,
-    detailComplete: ({result}) => JSON.parse(result),
-    videoInjectCode: \`
-      const iframeSrc = document.querySelector('iframe').src;
-      const videoUrl = iframeSrc.match(/url=(.*)/)[1];
-      window.ReactNativeWebView.postMessage(JSON.stringify({videoUrl}));
-    \`,
-    videoComplete: ({result}) => JSON.parse(result),
-}
-`;
-
 let plugins: Array<Plugin> = [];
 let currentPlugin: Plugin | null = null;
 const pluginStateMapper = new StateMapper(() => plugins);
